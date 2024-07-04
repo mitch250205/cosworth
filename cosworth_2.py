ac8dd4623deec87ba342dfd5da7d04c80dfdb65d
Paul Mitchell <paulm.jkp@gmail.com>
2024-07-04 19:31:18 +0100

    Feature/201 add uart output (#1)
    * removing comment 1
    
    * removing comment 2
    
    * removing comment 3 and two lines
    
    * removing comment
    
    * removing line
    
    * removing 2nd  line
    
    * removing line and comment
    
    ---------
    
    Co-authored-by: Paul Mitchell <pmitchell@intermedia.com>

--- cosworth_2.py
+++ cosworth_2.py
@@ -9,17 +9,11 @@ def linux_version(version_string):
     with open('output.log', 'r') as file:
         log_content = file.read()
 
-    # regex pattern to extract the OS version
-    # os_version_pattern = re.compile(r'OS Version\s*:\s*(kappa \d+\.\d+\.\d+ \(Release\) [A-Za-z]+\s\d+\s\d{4})')
-    # Define the regex pattern
-
 
     pattern = r"OS Version\s*:\s*(.*)"
 
     # Search for the pattern in the text
     match = re.search(pattern, log_content)
-
-    # Check if a match was found and print the result
     if match:
         os_version = match.group(1)
         print(f"OS Version: {os_version}")
@@ -28,14 +22,12 @@ def linux_version(version_string):
         print("OS Version not found.")
         status = False
 
-
     return status
 
 @pytest.fixture()
 def search_for_linux_version(request):
     versionString = request.param
     return linux_version(versionString)
-
 
 
 @pytest.mark.parametrize('search_for_linux_version', ["OS Version\s*:"], indirect=['search_for_linux_version'])
