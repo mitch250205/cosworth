b4052aecf8bd5a38ee215efe376d0a55ef019b6e
Paul Mitchell <pmitchell@intermedia.com>
2024-07-04 18:58:14 +0100

    adding answer for test 2

---
+++ cosworth_2.py
@@ -0,0 +1,43 @@
+import pytest
+import re
+
+
+def linux_version(version_string):
+    print(version_string)
+
+    # Read the content of the log file
+    with open('output.log', 'r') as file:
+        log_content = file.read()
+
+    # regex pattern to extract the OS version
+    # os_version_pattern = re.compile(r'OS Version\s*:\s*(kappa \d+\.\d+\.\d+ \(Release\) [A-Za-z]+\s\d+\s\d{4})')
+    # Define the regex pattern
+
+
+    pattern = r"OS Version\s*:\s*(.*)"
+
+    # Search for the pattern in the text
+    match = re.search(pattern, log_content)
+
+    # Check if a match was found and print the result
+    if match:
+        os_version = match.group(1)
+        print(f"OS Version: {os_version}")
+        status = True
+    else:
+        print("OS Version not found.")
+        status = False
+
+
+    return status
+
+@pytest.fixture()
+def search_for_linux_version(request):
+    versionString = request.param
+    return linux_version(versionString)
+
+
+
+@pytest.mark.parametrize('search_for_linux_version', ["OS Version\s*:"], indirect=['search_for_linux_version'])
+def test_search_for_linux_version(search_for_linux_version):
+    assert search_for_linux_version == True
